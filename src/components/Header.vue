eb9a5e9c534cd76b01fb67b0ec13df3cd00bd6f3
RDilly <ryan.dillinger@hey.com>
2021-03-04 07:34:39 -0600

    creating some components

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app" class="header">I'm a header.</div>
+</template>
+
+<style>
+.header {
+  font-size: large;
+  font-family: Arial, Helvetica, sans-serif;
+  padding: 1em;
+  margin: 1em;
+  border-bottom: 1px solid black;
+  background: #eee;
+}
+</style>
---
+++ src/components/Names.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    {{ name }} <br/>
+    {{ nameIsGreat }} <br/>
+    <input
+    v-model="name"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title'],
+name: 'Names',
+data: () => ({
+  name: "foo"
+}),
+computed: {
+  nameIsGreat() {
+    return this.name + " is great"
+  }
+}
+
+};
+</script>
+
+<style scoped></style>
